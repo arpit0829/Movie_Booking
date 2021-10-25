12396ee4cdc7522c8bb961fe35d6b2e0e65b21ac
arpit0829 <arpit.agarwal@ranosys.com>
2021-10-25 10:01:20 +0530

    Movie hall details

---
+++ Movie.java
@@ -0,0 +1,11 @@
+class Movie{
+    public static final String[] Movie_Hall = {"Audi - 1","Audi - 2","Audi - 3"};
+    public String[] movieName = {"Pokemon","CHEHRE  ","F&F 9  "}; 
+    public static String[] Movie_Genre = {"Cartoon","Romentic","Action"};
+    public static final int No_Of_Seats[] = {50,75,100};
+    public int price[] = {100,150,200};
+    public Movie() {
+    	System.out.println("Welcome to Inox Sunny Trade Center !!");
+    	
+    }
+}
